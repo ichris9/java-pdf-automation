bb0957a3ddecff48089e52a176b7978161479584
ichris9 <christian.h2k6@gmail.com>
2025-11-21 11:20:52 -0300

    Método padrão de busca
    Pattern e Matcher

--- MgtBolina_project/src/main/java/com/mycompany/mgtbolina_project/pdf_coletor_dados.java
+++ MgtBolina_project/src/main/java/com/mycompany/mgtbolina_project/pdf_coletor_dados.java
@@ -1,7 +1,26 @@
 package com.mycompany.mgtbolina_project;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class pdf_coletor_dados {
     
+    //método auxiliar de busca para usar o princípio Dont-Repeat-Yourself
+    private String FindFistGroup(String text, String regex){
+        // Flags: CASE_INSENSITIVE (ignora maiúsculas/minúsculas) e DOTALL (inclui quebras de linha no '.')
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(text);
+        
+        if(matcher.find()){
+            //retorna o primeiro grupo de captura
+            return matcher.group(1).trim();
+        }
+        return "Não Aplicavel (N/A)";//campo não encontrado
+    }
+    
+    
+    
     public String ExtractDanfeNumber(String texto){
-        return texto;
+       
     }
 }
