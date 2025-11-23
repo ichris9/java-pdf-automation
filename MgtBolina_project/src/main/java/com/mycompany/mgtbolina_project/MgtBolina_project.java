4fadd14c827be2c138c6d870191a37434c91f2b6
ichris9 <christian.h2k6@gmail.com>
2025-11-22 21:32:17 -0300

    teste: NumNota e Total funcionando

--- MgtBolina_project/src/main/java/com/mycompany/mgtbolina_project/MgtBolina_project.java
+++ MgtBolina_project/src/main/java/com/mycompany/mgtbolina_project/MgtBolina_project.java
@@ -1,7 +1,32 @@
 package com.mycompany.mgtbolina_project;
 
+import java.io.File;
+
 public class MgtBolina_project {
     public static void main(String[] args) {
-        //main
+       File pdfFile = new File("C:\\Users\\chris\\Downloads\\notafiscal-notebook.pdf");
+        
+       //lê o pdf
+       Pdf_Leitor leitor = new Pdf_Leitor();
+       
+       //pega todo o texto o pdf
+       String textoBruto = leitor.ExtractText(pdfFile.getPath());
+        
+       
+       if(textoBruto != null){
+         
+           pdf_coletor_dados coletor = new pdf_coletor_dados();
+           
+           //texto brto para extrair dados com regex
+           String numNota = coletor.ExtractDanfeNumber(textoBruto);
+           String numTotal= coletor.ExtractTotalNumber(textoBruto);
+           String placaVeiclo = coletor.ExtracPlacaVeiculo(textoBruto);
+           
+           System.out.println(">>>DADOS DA NOTA<<<");
+           System.out.println("Numero da Nota: " + numNota);
+          // System.out.println("Placa veiculo: " + placaVeiclo);
+           System.out.println("Total da nota: "+ numTotal);
+       }
+
     }
 }
--- MgtBolina_project/src/main/java/com/mycompany/mgtbolina_project/pdf_coletor_dados.java
+++ MgtBolina_project/src/main/java/com/mycompany/mgtbolina_project/pdf_coletor_dados.java
@@ -8,32 +8,32 @@ public class pdf_coletor_dados {
     //método auxiliar de busca para usar o princípio Dont-Repeat-Yourself
     private String FindFistGroup(String text, String regex){
         // Flags: CASE_INSENSITIVE (ignora maiúsculas/minúsculas) e DOTALL (inclui quebras de linha no '.')
-        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE );
         Matcher matcher = pattern.matcher(text);
         
         if(matcher.find()){
             //retorna o primeiro grupo de captura
             return matcher.group(1).trim();
         }
-        return "Não Aplicavel (N/A)";//campo não encontrado
+        return "Não Encontrada (N/E) ou Vazio";//campo não encontrado
     }
     
     
     
     public String ExtractDanfeNumber(String textoCompleto){
-       String regex = "N°\\s+([\\d\\.]+)";
+       String regex = "[1\\s\\-\\s SAÍDA\\n]([\\d]{6,6})";
        
        return FindFistGroup(textoCompleto, regex);
     }
     
     public String ExtractTotalNumber(String textoCompleto){
-        String regex = "VALOR TOTAL DA NOTA[\\s\\n]+([\\d\\.,]+)";
+        String regex = "VALOR TOTAL DOS PRODUTOS[\\s+\\n+]+([\\d\\.,]+)";
         
         return FindFistGroup(textoCompleto, regex);
     }
     
     public String ExtracPlacaVeiculo(String textoCompleto){
-        String regex = "PLACA DO VEÍCULO[\\s\\n]+([A-Z0-9]{7,8})";
+        String regex = "PLACA DO VEÍCULO[\\s+\\n]+([A-Z0-9]{7,7})";
         
         return FindFistGroup(textoCompleto, regex);
     }
